a7d80c055bb5010811d84ea40ff1be4cf92558b3
lovelygiants <tonyvllegas@gmail.com>
2018-07-03 12:28:43 -0500

    primera versión

---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+package.estructuradatos;
+
+public.class.HolaMundo.{
+
+	public.static.void.main(string[] args){
+		System.out.printin("hola taller");
+		new java.util.scanner(system.in).nextline();}}
